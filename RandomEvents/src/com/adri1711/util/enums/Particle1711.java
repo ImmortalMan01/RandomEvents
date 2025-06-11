1d059d1933c315beee5bb2f72e43e7afb1da664f
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-11 04:50:13 +0300

    Handle unknown particle configs safely

--- RandomEvents/src/com/adri1711/util/enums/Particle1711.java
+++ RandomEvents/src/com/adri1711/util/enums/Particle1711.java
@@ -2,5 +2,16 @@ package com.adri1711.util.enums;
 
 public enum Particle1711 {
     DEFAULT,
-    REDSTONE
+    REDSTONE;
+
+    public static Particle1711 safeValueOf(String name) {
+        if (name == null) {
+            return DEFAULT;
+        }
+        try {
+            return Particle1711.valueOf(name.trim().toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            return DEFAULT;
+        }
+    }
 }
