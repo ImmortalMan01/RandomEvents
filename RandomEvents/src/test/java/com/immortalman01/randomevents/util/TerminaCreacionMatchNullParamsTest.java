c80da7ee3b1ba638e972573d23b428e5aa8b35e3
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-19 14:24:42 +0300

    Handle null player and object in termination methods

---
+++ RandomEvents/src/test/java/com/immortalman01/randomevents/util/TerminaCreacionMatchNullParamsTest.java
@@ -0,0 +1,19 @@
+package com.immortalman01.randomevents.util;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.immortalman01.randomevents.RandomEvents;
+
+public class TerminaCreacionMatchNullParamsTest {
+    @Test
+    public void doesNotThrowWithNullPlayerAndMatch() {
+        RandomEvents plugin = Mockito.mock(RandomEvents.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(plugin.getLoggerP()).thenReturn(Logger.getLogger("test"));
+        assertDoesNotThrow(() -> UtilsRandomEvents.terminaCreacionMatch(plugin, null, null));
+    }
+}
