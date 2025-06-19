83ad40abd197a3268df1dcabad29eb867f959bf9
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-19 15:05:44 +0300

    Handle full spawn lists

--- RandomEvents/src/com/immortalman01/randomevents/language/LanguageMessages.java
+++ RandomEvents/src/com/immortalman01/randomevents/language/LanguageMessages.java
@@ -156,7 +156,8 @@ public class LanguageMessages {
 
 	private String matchJoin;
 	private String matchLeave;
-	private String lacksInfoCreation;
+        private String lacksInfoCreation;
+        private String tooManySpawns;
 	private String shrink;
 	private String warmupEnd;
 	private String playersMove;
@@ -3211,9 +3212,35 @@ public class LanguageMessages {
 		return s;
 	}
 
-	public void setLacksInfoCreation(String lacksInfoCreation) {
-		this.lacksInfoCreation = lacksInfoCreation;
-	}
+        public void setLacksInfoCreation(String lacksInfoCreation) {
+                this.lacksInfoCreation = lacksInfoCreation;
+        }
+
+        public String getTooManySpawns() {
+                String s = tooManySpawns;
+                try {
+                        Matcher match = pattern.matcher(s);
+                        Map<String, ChatColor> mapa = new HashMap<String, ChatColor>();
+                        while (match.find()) {
+                                String color = s.substring(match.start() + 1, match.end());
+                                Method method = ChatColor.class.getMethod("of", String.class);
+                                ChatColor chatc = (ChatColor) method.invoke(null, color);
+                                mapa.put("&" + color, chatc);
+                        }
+                        for (Entry<String, ChatColor> ent : mapa.entrySet()) {
+                                s = s.replaceAll(ent.getKey(), ent.getValue() + "");
+                        }
+                        s = ChatColor.translateAlternateColorCodes('&', s);
+                } catch (Exception e) {
+                        s = s.replaceAll("&", "§");
+                }
+                s = s.replaceAll("\\\\n", Constantes.SALTO_LINEA);
+                return s;
+        }
+
+        public void setTooManySpawns(String tooManySpawns) {
+                this.tooManySpawns = tooManySpawns;
+        }
 
 	public String getShrink() {
 		String s = shrink;
